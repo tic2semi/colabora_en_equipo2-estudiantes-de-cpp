4d67a8c011070afb31ce7109a9ec091b625a5a85
Juan Francisco Cortejosa Galindo <jcorgal946@g.educaand.es>
2022-03-04 16:43:10 +0100

    tarea 2
    RESTA

--- I3_calculadora.cpp
+++ I3_calculadora.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
 
-//Tarea1 (David Basallote): Añadir funcion para sumar, 
+//Tarea1 (David Basallote): Aï¿½adir funcion para sumar, 
 //y modificar el la opcion 1 del switch para que
-//invoque a la funcion sumar con los parámetros correctos.
+//invoque a la funcion sumar con los parï¿½metros correctos.
 
-//Tarea2: Añadir funcion para restar
+//Tarea2: Aï¿½adir funcion para restar
 //y modificar el la opcion 2 del switch para que
-//invoque a la funcion restar con los parámetros correctos.
+//invoque a la funcion restar con los parï¿½metros correctos.
 
-//Tarea3: Añadir función para multiplicar
+//Tarea3: Aï¿½adir funciï¿½n para multiplicar
 //y modificar el la opcion 3 del switch para que
-//invoque a la funcion multipliar con los parámetros correctos.
+//invoque a la funcion multipliar con los parï¿½metros correctos.
 
-//Tarea4: Añadir función para dividir
+//Tarea4: Aï¿½adir funciï¿½n para dividir
 //y modificar el la opcion 4 del switch para que
-//invoque a la funcion dividir con los parámetros correctos.
+//invoque a la funcion dividir con los parï¿½metros correctos.
 
 //Tarea5: Modificar el programa para que el usuario pueda realizar
-//tantas operaciones como quiera, hasta que indique que no necesita más.
+//tantas operaciones como quiera, hasta que indique que no necesita mï¿½s.
 
-//Tarea6: añadir la función 'prepara_operación', que lee los operandos
-//n1 y n2, y muestra el menú en pantalla.
+//Tarea6: aï¿½adir la funciï¿½n 'prepara_operaciï¿½n', que lee los operandos
+//n1 y n2, y muestra el menï¿½ en pantalla.
 
 int sumar(int numero1, int numero2)
 {
@@ -36,27 +36,27 @@ int main(int argc, char *argv[]) {
 	char otra_operacion;
 	cout << "Bienvenido al programa calculador.\n";	
 
-	cout << "Introduce dos números: \n";
+	cout << "Introduce dos nï¿½meros: \n";
 	cin >> n1;
 	cin >> n2;		
 	suma=sumar(n1, n2);
 	
-	//Muestra menú de opciones
+	//Muestra menï¿½ de opciones
 	cout	
-		<< "Éstas son las opciones: \n"
+		<< "ï¿½stas son las opciones: \n"
 		<< "1) Sumar " << n1 << "+" << n2 << "\n" 
 		<< "2) Restar " << n1 << "-" << n2 << "\n" 
 		<< "3) Multiplicar " << n1 << " x " << n2 << "\n" 
 		<< "4) Dividir " << n1 << " / " << n2 << "\n";
 	
-	//Asegura que introduce opción válida
+	//Asegura que introduce opciï¿½n vï¿½lida
 	do {
-		cout << "Introduce una opción entre 1 y 4: \n";
+		cout << "Introduce una opciï¿½n entre 1 y 4: \n";
 		cin >> opcion;
 	}while (opcion<1 || opcion>4);
 	
-	//Según la opción introducida, realiza el cálculo correcto
-	cout << "Ha elegido la opción " << opcion << "\n";
+	//Segï¿½n la opciï¿½n introducida, realiza el cï¿½lculo correcto
+	cout << "Ha elegido la opciï¿½n " << opcion << "\n";
 	switch (opcion) {
 	case 1:
 		cout << n1 << "+" << n2 << "=" << suma <<"\n";
